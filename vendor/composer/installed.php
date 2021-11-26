9ff9eccc6e0391682f4fba7bd3d0fad79e6f16fe
setiawanwcksn <60248993+setiawanwcksn@users.noreply.github.com>
2021-11-26 16:40:15 +0700

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'setiawan/task-composer-akademi-batch-2',
+        'reference' => 'ddbdb10522c10dab56542ec31802d3d5c8fb6636',
+        'name' => 'setiawan-wcksn/task-composer-akademi-batch-2',
         'dev' => true,
     ),
     'versions' => array(
-        'setiawan/task-composer-akademi-batch-2' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'setiawan-wcksn/task-composer-akademi-batch-2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'ddbdb10522c10dab56542ec31802d3d5c8fb6636',
             'dev_requirement' => false,
         ),
     ),
